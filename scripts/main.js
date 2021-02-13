5accd370f3464dd1f6b2706543c2a2149bb3167a
VLI77 <vincent.li@hetic.net>
2021-02-13 17:49:00 +0100

    animation sun

--- scripts/main.js
+++ scripts/main.js
@@ -9,9 +9,6 @@ sizes.width = window.innerWidth
 sizes.height = window.innerHeight
 
 
-
-
-
 const cursorParallaxElements = document.querySelectorAll('.js-cursor-parallax')
 
 document.addEventListener('mousemove', (_event) =>
@@ -22,12 +19,12 @@ document.addEventListener('mousemove', (_event) =>
 
    cursorParallaxElements.forEach((_element, _key) =>
    {
-     const strength = _key / (cursorParallaxElements.length - 1)
-     const translateX = -ratioX * 10 * strength
-     const translateY = -ratioY * 10 * strength
+     const translateX = -ratioX * 10 
+     const translateY = -ratioY * 10  
 
 
      _element.style.transform = `translateX(${translateX}%) translateY(${translateY}%`
+     _element.style.transition = "all 0.1s"
 
    })
 })
@@ -35,6 +32,8 @@ document.addEventListener('mousemove', (_event) =>
 
 
 const scrollParallaxElements =  document.querySelectorAll('.js-scroll-parallax')
+
+
 
 window.addEventListener('scroll', () =>
 {
@@ -52,13 +51,11 @@ window.addEventListener('scroll', () =>
 
 
 
-
-
-var isInViewport = function (elem) {
+let isInViewport = function (elem) {
     var distance = elem.getBoundingClientRect();
     
 	return (
-        distance.top >= 0 &&
+        distance.top >= 10 &&
 		distance.bottom <= (window.innerHeight +100 || document.documentElement.clientHeight+100) &&
         distance.right <= (window.innerWidth || document.documentElement.clientWidth) &&
         distance.left <= (window.innerWidth || document.documentElement.clientWidth)
@@ -67,23 +64,35 @@ var isInViewport = function (elem) {
     );
     
 };
+let isInViewport2 = function (elem) {
+	var distance = elem.getBoundingClientRect();
+	return (
+		distance.top >= -500 &&
+		distance.left >= 0 &&
+		distance.bottom <= (window.innerHeight+500 || document.documentElement.clientHeight+500) &&
+		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+};
+
+let project = document.querySelector('.casse_brique');
+let project2 = document.querySelector('.audio_player');
+let project3 = document.querySelector('.royal_noisiel');
+let project4 = document.querySelector('.dropshipping');
 
 
-var project = document.querySelector('.casse_brique');
-var project2 = document.querySelector('.audio_player');
-var project3 = document.querySelector('.royal_noisiel');
-var project4 = document.querySelector('.dropshipping');
+let description = document.querySelector('.description_1');
+let description2 = document.querySelector('.description_2');
+let description3 = document.querySelector('.description_3');
+let description4 = document.querySelector('.description_4');
+let main = document.querySelector('.main');
+let sun = document.querySelector('.sun');
+let cloud = document.querySelectorAll('.cloud')
+let cloud2 = document.querySelector('.cloud2')
 
+console.log(cloud);
 
-var description = document.querySelector('.description_1');
-var description2 = document.querySelector('.description_2');
-var description3 = document.querySelector('.description_3');
-var description4 = document.querySelector('.description_4');
-
-
-
-let tab = [project, project2, project3, project4];
-let tab2 = [description, description2, description3, description4];
+let tab = [project, project3, project4];
+let tab2 = [description, description3, description4];
 
 
     window.addEventListener('scroll', function (event) {
@@ -93,12 +102,41 @@ let tab2 = [description, description2, description3, description4];
               tab[i].style.transition= "all 0.5s"
               tab2[i].style.transform ="translate(-30vw)"
               tab2[i].style.transition= "all 0.5s"
+
+
             } else {
                 tab[i].style.transform ="translate(-30vw)"
                 tab[i].style.transition= "all 0.5s"
                 tab2[i].style.transform ="translate(30vw)"
                 tab2[i].style.transition= "all 0.5s"
+
+
             }
           }
           }, false)
           
+          window.addEventListener('scroll', function (event) {
+         
+            if (!isInViewport2(main)) {
+                sun.style.transform ="translate(30vw)"
+                sun.style.transition= "all 0.5s"
+
+                for (let i = 0;  i < cloud.length; i++) {
+                    cloud[i].style.transform ="translate(30vw)"
+                    cloud[i].style.transition= "all 0.5s"  
+                    cloud2.style.transform ="translate(-30vw)"
+                    cloud2.style.transition= "all 0.5s"   
+                }      
+  
+            }else{
+                sun.style.transform ="translate(0vw)"
+                sun.style.transition= "all 0.5s"
+                for (let i = 0;  i < cloud.length; i++) {
+                    cloud[i].style.transform ="translate(0vw)"
+                    cloud[i].style.transition= "all 0.5s" 
+                    cloud2.style.transform ="translate(0vw)"
+                    cloud2.style.transition= "all 0.5s"   
+                }      
+  
+            }
+          }, false)
