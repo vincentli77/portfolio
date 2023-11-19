77845c320e12bb2a7fec0002ff3a6cf1fce7d9ca
Vincent LI <li.jiajinvincent1997@gmail.com>
2023-11-20 00:57:07 +0100

    css fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 import './style/index.scss'
-import { BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <Router>
+    <div className="main"></div>
     <App />
   </Router>,
   document.getElementById('root')
-);
-
+)
